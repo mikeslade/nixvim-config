5767a963234306b07dcf1767743f9c42a606ff39
Michael Slade <mikeslade@gmail.com>
2024-08-29 16:42:27 -0500

    nix flake update fix errors

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -1,23 +1,25 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      html = [["prettierd" "prettier"]];
-      css = [["prettierd" "prettier"]];
-      javascript = [["prettierd" "prettier"]];
-      javascriptreact = [["prettierd" "prettier"]];
-      typescript = [["prettierd" "prettier"]];
-      typescriptreact = [["prettierd" "prettier"]];
-      python = ["black"];
-      lua = ["stylua"];
-      nix = ["alejandra"];
-      markdown = [["prettierd" "prettier"]];
-      yaml = ["yamlfmt"];
+    settings = {
+      formatOnSave = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      formattersByFt = {
+        html = [["prettierd" "prettier"]];
+        css = [["prettierd" "prettier"]];
+        javascript = [["prettierd" "prettier"]];
+        javascriptreact = [["prettierd" "prettier"]];
+        typescript = [["prettierd" "prettier"]];
+        typescriptreact = [["prettierd" "prettier"]];
+        python = ["black"];
+        lua = ["stylua"];
+        nix = ["alejandra"];
+        markdown = [["prettierd" "prettier"]];
+        yaml = ["yamlfmt"];
+      };
+      notifyOnError = true;
     };
   };
 }
